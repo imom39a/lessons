66f9edbcafa358890ac3ff71e56d929e1ed81c31
imom39a <vin.omega@gmail.com>
2016-03-15 08:34:30 -0400

    hapi ????

---
+++ hapi_lessons/hapi_01/index.js
@@ -0,0 +1,26 @@
+'use strict'
+const Hapi = require('hapi')
+
+const server = new Hapi.Server()
+server.connection({
+    host: 'localhost',
+    port: 8000
+})
+
+server.route({
+        method: 'GET',
+        path: '/',
+        handler: (request, reply) => {
+        reply('hello hapi!')
+    }
+})
+
+server.route({
+        method: 'GET',
+        path: '/{name}',
+        handler: (request, reply) => {
+        reply(`hello ${request.params.name}!`)
+}
+})
+
+server.start(() => console.log('Started at:', server.info.uri))
